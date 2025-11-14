0c726078a22254c066cf69aa45c3ce4992b31782
davidsousadev <davidk1k3k@gmail.com>
2025-11-13 22:18:59 -0300

    Atualizar função loadScore para carregar score diretamente da API e ajustar mensagem de carregamento

--- app.js
+++ app.js
@@ -1,29 +1,24 @@
+// app.js
+
 const resultDiv = document.getElementById("resultado");
 
 async function loadScore() {
     try {
-        resultDiv.innerHTML = "<em>Carregando variáveis...</em>";
-       
-        const envRes = await fetch("https://apisme.vercel.app/testeme");
-        const envData = await envRes.json();
+        resultDiv.innerHTML = "<em>Carregando score...</em>";
 
-        const API_URL = envData.API_URL;
-        
+        const API_URL = "https://apisme.vercel.app/testeme/score";
+
+        const res = await fetch(API_URL);
+        const data = await res.json();
+
         resultDiv.innerHTML = `
-            <strong>API_URL carregada:</strong><br>
+            <strong>GET:</strong><br>
             <pre>${API_URL}</pre>
             <br>
-            <strong>Carregando Score...</strong>
-        `;
-       
-        const scoreRes = await fetch(`${API_URL}/testeme/score`);
-        const scoreData = await scoreRes.json();
 
-        resultDiv.innerHTML = `
             <strong>Resposta da API:</strong><br>
-            <pre>${JSON.stringify(scoreData, null, 2)}</pre>
+            <pre>${JSON.stringify(data, null, 2)}</pre>
         `;
-
     } catch (err) {
         console.error(err);
         resultDiv.innerHTML = `
